cb8ff965031e069b90e2de76588a94ba058bba33
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-10-21 23:46:52 +0530

    disbled same site in cookies

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,7 +25,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1hr" });
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
