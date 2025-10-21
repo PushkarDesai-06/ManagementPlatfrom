d03f616dd475ef15b12c80bd63e058e17f507946
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-10-21 16:24:44 +0530

    fixed url in cors middleware

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://semi-notion.vercel.app/",
+      "https://semi-notion.vercel.app",
     ],
     credentials: true,
   })
